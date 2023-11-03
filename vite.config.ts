d9a7801cc8e9bab9b342a34c6b1621b2fde628b8
ruanlsdn <ruanlsdn@gmail.com>
2023-11-03 18:24:54 -0300

    refactor: add propriedade p usar hot reload em dsv no docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
-    port: 3000,
+    port: 3002,
+    watch: {
+      usePolling: true,
+    }
   },
   plugins: [react()],
-});
+})
